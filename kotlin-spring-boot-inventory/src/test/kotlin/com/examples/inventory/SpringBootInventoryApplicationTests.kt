2d230d3b089f50040d4023043b353d71eed99242
Leif Stolberg <leif.stolberg@enableyou.de>
2025-02-08 13:23:58 +0100

    add test code

---
+++ kotlin-spring-boot-inventory/src/test/kotlin/com/examples/inventory/SpringBootInventoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.examples.inventory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootInventoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
